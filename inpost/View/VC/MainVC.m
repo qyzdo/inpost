fd393e6885ad5ac3869a54d5f5bc8de382d43d0b
Oskar Figiel <qyzdo@outlook.com>
2020-08-06 00:09:03 +0200

    Add missing init to alloc

--- inpost/View/VC/MainVC.m
+++ inpost/View/VC/MainVC.m
@@ -28,7 +28,7 @@
     
     NSArray *trackingNumbers = @[@"687100708024170011003255", @"687100218024170137482623", @"600441497024170126732236", @"642202397024170117575245", @"663410197024170119003197", @"682300297024170014391380"];
     
-    ApiCaller *apiCaller = [ApiCaller alloc];
+    ApiCaller *apiCaller = [[ApiCaller alloc] init];
     for (int i = 0; i < [trackingNumbers count]; i++)
     {
         [apiCaller downloadData:[trackingNumbers objectAtIndex:i] :self.parcelsArray completion:^(NSMutableArray *parcelList) {
